82061fcfa4be709ad49f0c5fea1a28de2e2c6fa3
sayananbu <67857676+sayananbu@users.noreply.github.com>
2023-06-27 14:01:07 +0300

    Update productviewscreen.styles.ts

--- src/components/screen/ProductViewScreen/styles/productviewscreen.styles.ts
+++ src/components/screen/ProductViewScreen/styles/productviewscreen.styles.ts
@@ -6,10 +6,17 @@ export const SProductViewContainer = styled.div`
     grid-template-columns: 50% 50%;
     grid-template-rows: 40% 50% 10%;
     width: 80%;
-    height: 80vh;
+    min-height: 80vh;
     margin: 0 auto;
     padding: 20px 0;
     place-items: center;
+	@media (max-width:425px) {
+		width: 95%;
+		grid-template-columns: 100%;
+    	grid-template-rows: auto auto auto 50px;
+		row-gap: 10px;
+		font-size: 13px;
+	}
 `;
 export const SImageView = styled.img`
     grid-row: 1 / span 2;
@@ -19,17 +26,30 @@ export const SImageView = styled.img`
     width: 70%;
     height: 70%;
     object-fit: contain;
+	@media (max-width:425px) {
+		grid-row: 1;
+    	grid-column: 1;
+	}
 `;
 
 export const SFullDescription = styled(SCardDescription)`
     grid-row: 2;
     grid-column: 2;
     font-size: 1.5rem;
+	@media (max-width:425px) {
+		grid-row: 3;
+    	grid-column: 1;
+		font-size: 1.2rem;
+	}
 `;
 export const SFullTitle = styled(SCardTitle)`
     grid-row: 1;
     grid-column: 2;
     font-size: 1.5rem;
+	@media (max-width:425px) {
+		grid-row: 2;
+    	grid-column: 1;
+	}
 `;
 export const SFooterContainer = styled.div`
     display: grid;
@@ -39,15 +59,24 @@ export const SFooterContainer = styled.div`
     grid-column: 1 / span 2;
     width: 100%;
     height: 100%;
+	overflow:hidden;
+	border-radius: 15px;
+	@media (max-width:425px) {
+		grid-row: 4;
+    	grid-column: 1;
+	}
 `;
 
 export const SFullPrice = styled(SPrice)`
     grid-column: 2;
+	@media (max-width:425px) {
+		grid-row: 1;
+    	grid-column: 2;
+	}
 `;
 export const SFullDeleteButtonContainer = styled(SDeleteButtonContainer)`
     grid-column: 1;
     grid-row: 1;
-    border-radius: 15px 0 0 0;
 `;
 export const SProductError = styled.pre`
     font-size: 1.5rem;
